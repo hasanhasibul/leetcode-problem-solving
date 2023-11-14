fcfe674e7404ae2b54c34be2418fc9b5f5b8deec
hasanhasibul <hasanhasibul395@gmail.com>
2023-11-15 01:31:27 +0600

    decompress-run-length-encoded-list

--- decompressRLElist.js
+++ decompressRLElist.js
@@ -1,16 +1,18 @@
 const decompressRLElist = function (nums) {
-  const output =[]
+  const output = [];
 
   for (let i = 0; i < nums.length; i++) {
-        
+    if (nums[2 * i] && nums[2 * i + 1]) {
+      // console.log(nums[2 * i], nums[2 * i + 1]);
+      for (let k = 0; k < nums[2 * i]; k++) {
+        output.push(nums[2 * i + 1]);
+      }
+    }
   }
-
-
-
 
   return output;
 };
 
-const nums = [1, 2, 3, 4];
+const nums = [1, 1, 2, 3];
 
 console.log(decompressRLElist(nums));
