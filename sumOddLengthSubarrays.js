a832fea30595004235c3b3bfd41afb7e0acea1fc
hasanhasibul <hasanhasibul395@gmail.com>
2023-11-18 02:30:31 +0600

    another way explore to sum-of-all-odd-length-subarrays

--- sumOddLengthSubarrays.js
+++ sumOddLengthSubarrays.js
@@ -25,3 +25,20 @@ const sumOddLengthSubarrays = function (arr) {
 
 const nums = [1, 4, 2, 5, 3];
 console.log(sumOddLengthSubarrays(nums));
+
+//  anther way
+const anotherWaysumOddLengthSubarrays = function (arr) {
+  let output = 0;
+
+  for (let i = 1; i <= arr.length; i += 2) {
+    for (let j = 0; j <= arr.length - i; j++) {
+      for (let k = 0; k < i; k++) {
+        output += arr[j + k];
+      }
+    }
+  }
+
+  return output;
+};
+
+console.log(anotherWaysumOddLengthSubarrays([1, 4, 2, 5, 3]));
